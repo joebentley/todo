ffdb5a1a0a49eef574fde1cd3443669d0a3366e4
Joe D. Bentley <joebentley10@gmail.com>
2015-01-03 01:01:25 +0000

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Todo',
+      version='1.0',
+      description='Simple todo list app',
+      author='Joe Bentley',
+      author_email='joebentley10@gmail.com',
+      packages=['app'],
+      scripts=['todo']
+     )
